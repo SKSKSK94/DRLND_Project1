1d213d365532b110e9a5cb858216dada5198494e
SKSKSK94 <73100569+SKSKSK94@users.noreply.github.com>
2021-07-05 02:02:46 +0900

    2021-07-05

--- ddqn_with_per_agent.py
+++ ddqn_with_per_agent.py
@@ -15,12 +15,19 @@ GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
-END_TRAIN_THRESHOLD = 16.0
+END_TRAIN_THRESHOLD = 16.0 
+
+
+EPS = 1e-8              # PER epsilon
+ALPHA = 0.6             # PER alpha
+BETA_INIT = 0.4         # PER beta
+BETA_INCREASE = 0.005   # PER beta_increase
+BETA_MAX = 1.0          # PER beta_max
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
-    """Interacts with and learns from the environment.""" 
+    """Interacts with and learns from the environment."""
 
     def __init__(self, state_size, action_size, seed):
         """Initialize an Agent object.
@@ -46,15 +53,33 @@ class Agent():
         self.t_step = 0
     
     def step(self, state, action, reward, next_state, done):
+
+        ################## PER ##################
+        # calculate TD error 
+        state_tensor = torch.tensor(state).unsqueeze(dim=0).float().to(device)
+        action_tensor = torch.tensor([action]).unsqueeze(dim=0).to(device)
+        reward_tensor = torch.tensor(reward).unsqueeze(dim=0).float().to(device)
+        next_state_tensor = torch.tensor(next_state).unsqueeze(dim=0).float().to(device)
+        done_tensor = torch.tensor(done).unsqueeze(dim=0).float().to(device)
+        
+        best_action = torch.argmax(self.qnetwork_local(next_state_tensor), dim=-1).unsqueeze(dim=-1)
+        Q_target_next = torch.gather(input=self.qnetwork_target(next_state_tensor).detach(), dim=-1, index=best_action) # torch.gather : Gathers values along an axis specified by dim.
+        # print(self.qnetwork_local(state_tensor).shape)
+        # print(action_tensor.shape)
+        target = reward_tensor + GAMMA*Q_target_next*(1-done_tensor) ########## if done True then Q_target_next should be zero
+        current_value = torch.gather(input=self.qnetwork_local(state_tensor), dim=1, index=action_tensor) # torch.gather : Gathers values along an axis specified by dim.
+        TD_error = abs(target - current_value)
+        unnormalized_TD_error = pow(TD_error + EPS, ALPHA)
+
         # Save experience in replay memory
-        self.memory.add(state, action, reward, next_state, done)
+        self.memory.add(state, action, reward, next_state, done, unnormalized_TD_error.detach().cpu())
         
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
             if len(self.memory) > BATCH_SIZE:
-                experiences = self.memory.sample()
+                experiences = self.memory.PER_sample()
                 self.learn(experiences, GAMMA)
 
     def act(self, state, eps=0.):
@@ -85,20 +110,30 @@ class Agent():
             experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
             gamma (float): discount factor
         """
-        states, actions, rewards, next_states, dones = experiences
+        states, actions, rewards, next_states, dones, weights = experiences
 
         ## TODO: compute and minimize the loss
         "*** YOUR CODE HERE ***"
-        Q_target_next = torch.max(self.qnetwork_target(next_states).detach(), dim=-1)[0].unsqueeze(dim=-1)
-    
+        ################## Double DQN ##################
+        best_action = torch.argmax(self.qnetwork_local(next_states), dim=-1).unsqueeze(dim=-1)
+        Q_target_next = torch.gather(input=self.qnetwork_target(next_states).detach(), dim=-1, index=best_action) # torch.gather : Gathers values along an axis specified by dim.
+        ################## Double DQN ##################
+
         target = rewards + gamma*Q_target_next*(1-dones) ########## if done True then Q_target_next should be zero
         current_value = torch.gather(input=self.qnetwork_local(states), dim=1, index=actions) # torch.gather : Gathers values along an axis specified by dim.
 
         self.qnetwork_local.zero_grad()
-        loss = torch.mean(torch.sqrt((target - current_value)**2 + 1e-10))
+        loss = torch.mean(torch.sqrt(weights * (target - current_value)**2 + 1e-10))
         # loss = F.mse_loss(current_value, target)
         loss.backward()
         self.optimizer.step()    
+
+        ################## PER ##################
+        # updatae TD_error_array
+        unnormlized_TD_error = pow(abs(target - current_value).detach() + EPS, ALPHA)
+        self.memory.update_TD_error_array(unnormlized_TD_error)
+        ################## PER ##################
+
         "*** YOUR CODE HERE ***"  
         
 
@@ -118,9 +153,15 @@ class Agent():
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
+
+    ################## PER ##################
+    def update_beta(self):
+        self.memory.beta = min(self.memory.beta + self.memory.beta_increase, BETA_MAX)
+    ################## PER ##################
+    
     def train(self, env, n_episodes=1800, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
         """
-        Deep Q-Learning.    
+        Double Deep Q-Learning with PER.    
         Params
         ======
             n_episodes (int): maximum number of training episodes
@@ -149,6 +190,7 @@ class Agent():
                 state = next_state
                 score += reward
                 if done:
+                    self.update_beta()       ############################# PER beta update #############################
                     break 
             scores_window.append(score)       # save most recent score
             scores.append(score)              # save most recent score
@@ -156,9 +198,9 @@ class Agent():
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
             if i_episode % 100 == 0:
                 print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-            if np.mean(scores_window) >= 20.0:
+            if np.mean(scores_window) >= END_TRAIN_THRESHOLD:
                 print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
-                torch.save(self.qnetwork_local.state_dict(), 'saved_DQN_agent.pth')
+                torch.save(self.qnetwork_local.state_dict(), 'saved_DDQN_PER_agent.pth')
                 break
         return scores
 
@@ -166,7 +208,7 @@ class Agent():
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+    def __init__(self, action_size, buffer_size, batch_size, seed, beta_init=BETA_INIT, beta_increase=BETA_INCREASE):
         """Initialize a ReplayBuffer object.
 
         Params
@@ -176,20 +218,43 @@ class ReplayBuffer:
             batch_size (int): size of each training batch
             seed (int): random seed
         """
+        self.buffer_size = buffer_size
         self.action_size = action_size
         self.memory = deque(maxlen=buffer_size)  
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
+
+
+        ################## PER ##################
+        self.unnormalized_TD_error_array = np.array([])
+        self.beta = beta_init
+        self.beta_increase = beta_increase
+        ################## PER ##################
     
-    def add(self, state, action, reward, next_state, done):
+    def add(self, state, action, reward, next_state, done, unnormalized_TD_error):
         """Add a new experience to memory."""
         e = self.experience(state, action, reward, next_state, done)
+        ################## PER ##################
         self.memory.append(e)
-    
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
+        self.unnormalized_TD_error_array = np.append(self.unnormalized_TD_error_array, unnormalized_TD_error) if len(self.unnormalized_TD_error_array) < self.buffer_size else np.append(self.unnormalized_TD_error_array[1:], unnormalized_TD_error)
+        ################## PER ##################
+
+    def PER_sample(self):
+        """Prioritily sample a batch of experiences from memory."""
+        ################## PER ##################
+        # sampling based on priority prob
+        idx_array = np.arange(len(self.memory))
+        prob = self.unnormalized_TD_error_array / sum(self.unnormalized_TD_error_array)
+        self.chosen_idx_array = np.random.choice(idx_array, size=self.batch_size, p=prob)
+        experiences = [self.memory[idx] for idx in self.chosen_idx_array]
+
+        # calculate importance sampling weight
+        weights_all = pow(len(self.memory) * prob, - self.beta)
+        weights_all = weights_all / np.max(weights_all)
+        weights = torch.tensor([weights_all[idx] for idx in self.chosen_idx_array]).unsqueeze(dim=-1).to(device)
+
+        ################## PER ##################
 
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
@@ -197,7 +262,11 @@ class ReplayBuffer:
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
   
-        return (states, actions, rewards, next_states, dones)
+        return (states, actions, rewards, next_states, dones, weights)
+
+    def update_TD_error_array(self, unormlized_TD_error):
+        for numer_idx, idx in enumerate(self.chosen_idx_array):
+            self.unnormalized_TD_error_array[idx] = unormlized_TD_error[numer_idx]
 
     def __len__(self):
         """Return the current size of internal memory."""
